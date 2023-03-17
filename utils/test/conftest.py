9404083b42fc9a5f01328d57214d7537ee1ced95
IraSam <samy.iralour@gmail.com>
2023-03-17 20:50:12 +0000

    fixing connection details test

--- utils/test/conftest.py
+++ utils/test/conftest.py
@@ -2,6 +2,6 @@ import os
 
 
 def pytest_generate_tests(metafunc):
-    os.environ['database_port'] = '3306'
-    os.environ['database_user'] = 'my_username'
-    os.environ['database_pwd'] = 'my_pwd'
+    os.environ['db_port'] = '3306'
+    os.environ['db_user'] = 'my_username'
+    os.environ['db_pwd'] = 'my_pwd'
--- utils/test/test_connection_info.py
+++ utils/test/test_connection_info.py
@@ -2,4 +2,4 @@ from utils.connection_info import get_connection_details
 
 
 def test_get_connection_details():
-    assert get_connection_details('database') == {'port': '3306', 'user': 'my_username', 'pwd': 'my_pwd'}
+    assert get_connection_details('db') == {'name': 'db', 'port': '3306', 'user': 'my_username', 'pwd': 'my_pwd'}
